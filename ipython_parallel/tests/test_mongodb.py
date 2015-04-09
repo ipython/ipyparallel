78ac252e33c799aff96845d879cc5789d9dd7311
Min RK <benjaminrk@gmail.com>
2015-04-09 13:56:54 -0700

    skip mongodb tests without mongodb

--- ipython_parallel/tests/test_mongodb.py
+++ ipython_parallel/tests/test_mongodb.py
@@ -1,20 +1,7 @@
-"""Tests for mongodb backend
+"""Tests for mongodb backend"""
 
-Authors:
-
-* Min RK
-"""
-
-#-------------------------------------------------------------------------------
-#  Copyright (C) 2011  The IPython Development Team
-#
-#  Distributed under the terms of the BSD License.  The full license is in
-#  the file COPYING, distributed as part of this software.
-#-------------------------------------------------------------------------------
-
-#-------------------------------------------------------------------------------
-# Imports
-#-------------------------------------------------------------------------------
+# Copyright (c) IPython Development Team.
+# Distributed under the terms of the Modified BSD License.
 
 import os
 
@@ -22,35 +9,44 @@ from unittest import TestCase
 
 from nose import SkipTest
 
-from pymongo import Connection
-from ipython_parallel.controller.mongodb import MongoDB
-
 from . import test_db
 
-conn_kwargs = {}
-if 'DB_IP' in os.environ:
-    conn_kwargs['host'] = os.environ['DB_IP']
-if 'DBA_MONGODB_ADMIN_URI' in os.environ:
-    # On ShiningPanda, we need a username and password to connect. They are
-    # passed in a mongodb:// URI.
-    conn_kwargs['host'] = os.environ['DBA_MONGODB_ADMIN_URI']
-if 'DB_PORT' in os.environ:
-    conn_kwargs['port'] = int(os.environ['DB_PORT'])
+c = None
 
-try:
-    c = Connection(**conn_kwargs)
-except Exception:
-    c=None
+def setup():
+    global c
+
+    try:
+        from pymongo import Connection
+    except ImportError:
+        raise SkipTest()
+
+    conn_kwargs = {}
+    if 'DB_IP' in os.environ:
+        conn_kwargs['host'] = os.environ['DB_IP']
+    if 'DBA_MONGODB_ADMIN_URI' in os.environ:
+        # On ShiningPanda, we need a username and password to connect. They are
+        # passed in a mongodb:// URI.
+        conn_kwargs['host'] = os.environ['DBA_MONGODB_ADMIN_URI']
+    if 'DB_PORT' in os.environ:
+        conn_kwargs['port'] = int(os.environ['DB_PORT'])
+    
+    try:
+        c = Connection(**conn_kwargs)
+    except Exception:
+        c=None
+
+def teardown(self):
+    if c is not None:
+        c.drop_database('iptestdb')
 
 class TestMongoBackend(test_db.TaskDBTest, TestCase):
     """MongoDB backend tests"""
 
     def create_db(self):
+        from ipython_parallel.controller.mongodb import MongoDB
         try:
             return MongoDB(database='iptestdb', _connection=c)
         except Exception:
             raise SkipTest("Couldn't connect to mongodb")
 
-def teardown(self):
-    if c is not None:
-        c.drop_database('iptestdb')
