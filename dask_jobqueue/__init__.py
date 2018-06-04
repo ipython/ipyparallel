756bc2dbe83746e8852286dd531c62de30fc931f
Spencer Clark <skclark@princeton.edu>
2018-06-04 08:55:27 -0400

    Add MoabCluster to __init__.py

--- dask_jobqueue/__init__.py
+++ dask_jobqueue/__init__.py
@@ -1,6 +1,7 @@
 # flake8: noqa
 from . import config
 from .core import JobQueueCluster
+from .moab import MoabCluster
 from .pbs import PBSCluster
 from .slurm import SLURMCluster
 from .sge import SGECluster
