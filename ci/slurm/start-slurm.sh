99d14443d38e9ecfd96a863f980912323f424883
Guillaume EB <g.eynard.bontemps@gmail.com>
2018-06-25 23:57:40 +0200

    Fix start-slurm.sh while loop condition (#76)

--- ci/slurm/start-slurm.sh
+++ ci/slurm/start-slurm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker-compose up --build -d
-while [ `./register_cluster.sh | grep "sacctmgr: error" | wc -l` -ne 0 ]
+while [ `./register_cluster.sh 2>&1 | grep "sacctmgr: error" | wc -l` -ne 0 ]
   do
     echo "Waiting for SLURM cluster to become ready";
     sleep 2
