569e1b61347e527d67f821bbf1b4714cbde8760e
Min RK <benjaminrk@gmail.com>
2019-09-12 15:53:09 +0200

    make sure directory is on path
    and adjust numpy benchmark

--- profiling/profiling_code.py
+++ profiling/profiling_code.py
@@ -1,5 +1,26 @@
 import ipyparallel as ipp
 import numpy as np
+
+# ensure ipyparallel_master_project is on sys.path
+import os
+import sys
+
+master_project_parent = os.path.abspath(
+        os.path.join(
+            os.path.dirname(__file__),
+            os.pardir,
+            os.pardir, # '..'
+        )
+    )
+
+def add_to_path(path):
+    import sys
+    sys.path.insert(0, path)
+
+
+# add it to path before importing it
+add_to_path(master_project_parent)
+
 from ipyparallel_master_project.benchmarks.utils import echo
 
 
@@ -8,15 +29,19 @@ def profile_many_empty_tasks(lview, n):
 
 
 def profile_tasks_with_large_data(client, num_bytes):
-    client[:]['x'] = np.array([0] * num_bytes, dtype=np.int8)
+    for i in range(10):
+        client[:].apply_sync(echo(0), np.empty(num_bytes, dtype=np.int8))
 
 
 def run_profiling():
     client = ipp.Client(profile='asv')
+    # add it to path on the engines
+    client[:].apply_sync(add_to_path, master_project_parent)
     lview = client.load_balanced_view()
-    for x in range(1, 5):
-        profile_many_empty_tasks(lview, 10 ** x)
+    # for x in range(1, 5):
+    #     profile_many_empty_tasks(lview, 10 ** x)
     for x in range(1, 7):
+        print(x)
         profile_tasks_with_large_data(client, 20 ** x)
 
 
