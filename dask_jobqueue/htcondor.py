c1e0a21a32d909edaf8fc1afb5a1d49b43f5bc33
Loïc Estève <loic.esteve@ymail.com>
2019-10-14 17:19:47 +0200

    Support older HTCondor versions without -file argument (#351)

--- dask_jobqueue/htcondor.py
+++ dask_jobqueue/htcondor.py
@@ -19,9 +19,11 @@ class HTCondorJob(Job):
 Environment = "%(quoted_environment)s"
 Arguments = "%(quoted_arguments)s"
 Executable = %(executable)s
+
+Queue
 """.lstrip()
 
-    submit_command = "condor_submit -queue 1 -file"
+    submit_command = "condor_submit"
     cancel_command = "condor_rm"
     job_id_regexp = r"(?P<job_id>\d+\.\d+)"
 
