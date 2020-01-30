c0fd3d4144bf90d3324fcef4f1156f396db9f40c
Tom-Olav Bøyum <boyum90@gmail.com>
2020-01-30 13:29:46 +0100

    BroadcastSchedulerNonCoalescing working when taskscheduler is not launched

--- cluster_start.py
+++ cluster_start.py
@@ -14,7 +14,7 @@ def main():
         )
     ]
 
-    for i in range(16):
+    for i in range(1):
         ps.append(
             Popen(
                 ['ipengine', '--debug', '--profile=asv'],
--- explore/control_flow.py
+++ explore/control_flow.py
@@ -5,15 +5,15 @@ def main():
     client = ipp.Client(profile='asv')
     # direct_view = client[:]
     # direct_view_result = direct_view.apply(lambda x: x * 2, 13)
-    # load_balanced_view = client.load_balanced_view()
-    # result = load_balanced_view.apply(lambda x: x * 2, 13)
-    # print(result.get())
     # print(direct_view_result.get())
+    load_balanced_view = client.load_balanced_view()
+    result = load_balanced_view.apply(lambda x: x * 2, 13)
+    print(result.get())
     broad_cast_view = client.broadcast_view()
-    bc_result = broad_cast_view.apply(
+    print(broad_cast_view.apply(
         lambda x: f'The answer to Life, the Universe and Everything: {x * 2}', 21
-    )
-    print(bc_result.get())
+    ).get())
+
 
 
 if __name__ == '__main__':
