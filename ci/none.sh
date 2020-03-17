23b2a0256d96ee93ec99581b0be1f1ad03fd0a4e
Loïc Estève <loic.esteve@ymail.com>
2020-03-17 17:36:50 +0100

    MNT: small CI cleanup (#395)

--- ci/none.sh
+++ ci/none.sh
@@ -5,7 +5,6 @@ function jobqueue_before_install {
   ./ci/conda_setup.sh
   export PATH="$HOME/miniconda/bin:$PATH"
   conda install --yes -c conda-forge python=$TRAVIS_PYTHON_VERSION dask distributed flake8 black pytest pytest-asyncio
-  pip install git+https://github.com/dask/distributed@master --upgrade --no-deps
 }
 
 function jobqueue_install {
