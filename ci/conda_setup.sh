bc0807fb3bb2e68310a6ff9fb89199e2e4978ab5
Loïc Estève <loic.esteve@ymail.com>
2020-04-13 08:39:51 +0200

    CI: fix URL for miniconda download. (#412)

--- ci/conda_setup.sh
+++ ci/conda_setup.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 # Install miniconda
-wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
+wget http://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
 bash ~/miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 conda update conda --yes
