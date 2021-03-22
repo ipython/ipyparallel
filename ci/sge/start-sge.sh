c282545cdad006164ac8603046accff523287292
Loïc Estève <loic.esteve@ymail.com>
2021-03-22 14:01:11 +0100

    Fail early for SGE rather than waiting the CI max time (#492)

--- ci/sge/start-sge.sh
+++ ci/sge/start-sge.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 docker-compose up -d --no-build
+
+START=$(date +%s)
+MAX_WAIT_SECONDS=300
+
 while [ `docker exec sge_master qhost | grep lx26-amd64 | wc -l` -ne 2 ]
-  do
+do
+    if [[ $(($(date +%s) - $START)) -gt $MAX_WAIT_SECONDS ]]; then
+        echo "Exiting after failing to start the cluster in $MAX_WAIT_SECONDS seconds"
+        exit 1
+    fi
     echo "Waiting for SGE slots to become available";
     sleep 1
-  done
+done
+
 echo "SGE properly configured"
