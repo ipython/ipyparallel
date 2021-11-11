220b9331ef145635e4867ca1e1b3aa08b160f7fd
Min RK <benjaminrk@gmail.com>
2021-11-11 15:52:35 +0100

    adopt tbump

--- ipyparallel/_version.py
+++ ipyparallel/_version.py
@@ -1,2 +1,34 @@
-version_info = (8, 0, 0, "b1")
-__version__ = ".".join(map(str, version_info))
+import re
+
+__version__ = "8.0.0b1"
+
+# matches tbump regex in pyproject.toml
+_version_regex = re.compile(
+    r'''
+  (?P<major>\d+)
+  \.
+  (?P<minor>\d+)
+  \.
+  (?P<patch>\d+)
+  (?P<pre>((a|b|rc)\d+))?
+  (\.
+    (?P<dev>dev\d*)
+  )?
+  ''',
+    re.VERBOSE,
+)
+
+_version_fields = _version_regex.match(__version__).groupdict()
+version_info = tuple(
+    field
+    for field in (
+        int(_version_fields["major"]),
+        int(_version_fields["minor"]),
+        int(_version_fields["patch"]),
+        _version_fields["pre"],
+        _version_fields["dev"],
+    )
+    if field is not None
+)
+
+__all__ = ["__version__", "version_info"]
