87d0d931af97bae3a69e6b77c029294c49d10b30
Min RK <benjaminrk@gmail.com>
2023-07-08 16:35:16 -0700

    use pytest.warns to assert warnings

--- ipyparallel/tests/test_util.py
+++ ipyparallel/tests/test_util.py
@@ -1,22 +1,19 @@
 import socket
-from unittest import mock
 
+import pytest
 from jupyter_client.localinterfaces import localhost, public_ips
 
 from ipyparallel import util
 
 
-@mock.patch('warnings.warn')
-def test_disambiguate_ip(warn_mock):
+def test_disambiguate_ip():
     # garbage in, garbage out
     public_ip = public_ips()[0]
     assert util.disambiguate_ip_address('garbage') == 'garbage'
     assert util.disambiguate_ip_address('0.0.0.0', socket.gethostname()) == localhost()
     wontresolve = 'this.wontresolve.dns'
-    assert util.disambiguate_ip_address('0.0.0.0', wontresolve) == wontresolve
-    warn_mock.assert_called_once_with(
-        'IPython could not determine IPs for {}: '
-        '[Errno -2] Name or service not known'.format(wontresolve),
-        RuntimeWarning,
-    )
+    with pytest.warns(
+        RuntimeWarning, match=f"IPython could not determine IPs for {wontresolve}"
+    ):
+        assert util.disambiguate_ip_address('0.0.0.0', wontresolve) == wontresolve
     assert util.disambiguate_ip_address('0.0.0.0', public_ip) == localhost()
