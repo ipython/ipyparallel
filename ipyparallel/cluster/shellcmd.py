b8ee960e04628528c1f747e17486dbf56f688071
Johannes Otepka <johannes.otepka@tuwien.ac.at>
2023-10-16 09:19:17 +0200

    ssh cluster support for windows

--- ipyparallel/cluster/shellcmd.py
+++ ipyparallel/cluster/shellcmd.py
@@ -1,10 +1,150 @@
 #!/usr/bin/env python
-"""Shell helper application for OS independent shell commands"""
+"""
+Shell helper application for OS independent shell commands
+
+Currently the following command are supported:
+* start:   starts a process into background and returns its pid
+* running: check if a process with a given pid is running
+* kill:    kill a process with a given pid
+* mkdir:   creates directory recursively (no error if directory already exists)
+* rmdir:   remove directory recursively
+* exists:  checks if a file/directory exists
+* remove:  removes a file
+"""
 
 from subprocess import check_output, run, CalledProcessError
 from argparse import ArgumentParser
-import sys, re
+import sys, re, os
 import inspect
+import shlex
+
+
+class ShellCommandReceive:
+    """
+    Wrapper for receiving and performing ssh shell commands
+
+    All supported shell commands have a cmd_ prefix. When adding new functions make sure that the is an
+    equivalent in the ShellCommandSend class. When a command failed a non zero exit code will be returned.
+    Hence, the ShellCommandSend class always uses subprocess.check_output for assessing if the command was
+    successfull. Some command require information to be returned (cmd_exists, cmd_start, cmd_running) which
+    is written to stdout in the following form: __<key>=<value>__
+    """
+    def __init__(self):
+        self.debugging = False
+        pass
+
+    def cmd_start(self, start_cmd, env=None, output_file=None):
+        if env:
+            # add provided entries to environment
+            if isinstance(env,str):
+                env_dict = eval(env)
+                assert(isinstance(env_dict, dict) is True)
+                env = env_dict
+
+            assert(isinstance(env, dict) is True)   # make sure that env is a dictionary
+
+            # update environment
+            for key, value in env.items():
+                if value is not None and value != '':
+                    os.environ[key] = str(value)
+                else:
+                    # unset entry if needed
+                    if key in os.environ:
+                        del os.environ[key]
+
+        if self.debugging:
+            with open("start_cmd.txt", "w") as f:
+                f.write(start_cmd)
+
+        if os.name == "nt":
+            from subprocess import Popen
+            from subprocess import CREATE_NEW_CONSOLE
+            from subprocess import CREATE_BREAKAWAY_FROM_JOB
+
+            flags = 0
+            flags |= CREATE_NEW_CONSOLE
+            flags |= CREATE_BREAKAWAY_FROM_JOB
+
+            pkwargs = {
+                'close_fds': True,  # close stdin/stdout/stderr on child
+                'creationflags': flags
+            }
+            if output_file:
+                fo = open(output_file, "w")
+                pkwargs['stdout'] = fo
+                pkwargs['stderr'] = fo
+
+            p = Popen(start_cmd, **pkwargs)
+
+            print(f'__remote_pid={p.pid}__')
+        else:
+            if output_file:
+                nohup_start = f"nohup {start_cmd} >{output_file} 2>&1 </dev/null & echo __remote_pid=$!__"
+            else:
+                nohup_start = f"nohup {start_cmd} >/dev/null 2>&1 </dev/null & echo __remote_pid=$!__"
+            os.system(nohup_start)
+
+
+    def cmd_running(self, pid):
+        if os.name == "nt":
+            # taken from https://stackoverflow.com/questions/568271/how-to-check-if-there-exists-a-process-with-a-given-pid-in-python
+            import ctypes
+            PROCESS_QUERY_INFROMATION = 0x1000 # if actually PROCESS_QUERY_LIMITED_INFORMATION
+            STILL_ACTIVE = 259
+            processHandle = ctypes.windll.kernel32.OpenProcess(PROCESS_QUERY_INFROMATION, 0, pid)
+            if processHandle == 0:
+                print(f'__running=0__')
+            else:
+                i = ctypes.c_int(0)
+                pi = ctypes.pointer(i)
+                if ctypes.windll.kernel32.GetExitCodeProcess(processHandle, pi) == 0:
+                    print(f'__running=0__')
+                if i.value == STILL_ACTIVE:
+                    print(f'__running=1__')
+                else:
+                    print(f'__running=0__')
+                ctypes.windll.kernel32.CloseHandle(processHandle)
+        else:
+            ps_cmd = f'ps -p {pid} > /dev/null && echo "__running=1__" || echo "__running=0__"'
+            os.system(ps_cmd)
+
+    def cmd_kill(self, pid, sig=None):
+        if os.name == "nt":
+            # os.kill doesn't work reliable under windows. also see
+            # https://stackoverflow.com/questions/28551180/how-to-kill-subprocess-python-in-windows
+
+            # solution using taskill
+            #import subprocess
+            #subprocess.call(['taskkill', '/F', '/T', '/PID',  str(pid)])  # /T kills all child processes as well
+
+            # use windows api to kill process (doesn't kill children processes)
+            # To kill all children process things are more complicated. see e.g.
+            # http://mackeblog.blogspot.com/2012/05/killing-subprocesses-on-windows-in.html
+            import ctypes
+            PROCESS_TERMINATE = 0x0001
+            kernel32 = ctypes.windll.kernel32
+            processHandle = kernel32.OpenProcess(PROCESS_TERMINATE, 0, pid);
+            if processHandle:
+                kernel32.TerminateProcess(processHandle, 3)  # 3 is just an arbitrary exit code
+                kernel32.CloseHandle(processHandle)
+        else:
+            os.kill(pid, sig)
+
+    def cmd_mkdir(self, path):
+        os.makedirs(path, exist_ok=True)    # we allow that the directory already exists
+
+    def cmd_rmdir(self, path):
+        import shutil
+        shutil.rmtree(path)
+
+    def cmd_exists(self, path):
+        if os.path.exists(path):
+            print("__exists=1__")
+        else:
+            print("__exists=0__")
+
+    def cmd_remove(self, path):
+        os.remove(path)
 
 
 class ShellCommandSend:
@@ -20,16 +160,29 @@ class ShellCommandSend:
 
         The actual shell commands have a cmd_ prefix. When adding new functions make sure that the is an
         equivalent in the ShellCommandReceive class.
+
+        The sender class supports two modes for performing the actual shell command. The standard way by starting
+        the ipyparallel.cluster.shellcmd module requires the current code on the 'other side' of the shell
+        available. When developing or debugging it is more convenient to activate the 'use_code_sending' flag. This
+        transfers the ShellCommandReceive class directly to 'other side'. It should also be mentioned that the
+        code sending option is much faster in executing the code.
     """
     package_name = "ipyparallel.cluster.shellcmd"    # package name for send the command
     output_template = re.compile(r"__([a-z][a-z0-9_]+)=([a-z0-9\-\.]+)__", re.IGNORECASE)
+    receiver_code = inspect.getsource(ShellCommandReceive)
+    _python_chars_map =  str.maketrans( {"\\": "\\\\", "'": "\\'"} )
 
     def __init__(self, shell, args, python_path, use_code_sending = False):
         self.shell = shell
         self.args = args
         self.python_path = python_path
-        self.is_linux = None    # changed if get_remote_shell_info is called
-        self.use_code_sending = use_code_sending    # only activate when developing...
+        self.is_linux = None     # changed if get_remote_shell_info is called
+        self.is_powershell = None
+        self.join_params = True  # join all cmd params into a single param. does NOT work with windows cmd
+        self.use_code_sending = use_code_sending    # should be activated when developing...
+        self.debugging = False  # for outputs to file for easier debugging
+        if "ssh" in shell:
+            self.join_params = False    #just for testing
 
     def _check_output(self, cmd):
         return check_output(cmd).decode('utf8', 'replace')
@@ -49,18 +202,63 @@ class ShellCommandSend:
         else:
             raise Exception("Unknown command type")
 
+    def _format_for_python(self, param):
+        assert(isinstance(param, str) is True)
+        if param.isnumeric():
+            return param
+        else:
+            return f"'{param.translate(self._python_chars_map)}'"
+
+    def _powershell_quote(self, param):
+        if '"' in param or "'" in param or " " in param:
+            # we need to replace single and double quotes be two double quotes, but if we are inside a string,
+            # we need to prepend a backslash to the double quote. Otherwise it will get removed
+            quoted = ""
+            in_string = False
+            for idx, c in enumerate(param):
+                prev_c = None if idx == 0 else param[idx-1]
+                next_c = None if idx == len(param)-1 else param[idx+1]
+                if c == '"' and prev_c != "\\":
+                    in_string = not in_string
+                    quoted += '"'*2
+                    continue
+                if c == "'":
+                    if in_string:
+                        quoted += '\\"'*2
+                    else:
+                        quoted += '"'*2
+                    continue
+                quoted += c
+            return "'"+quoted+"'"
+        else:
+            return param
+
     def _send_cmd(self, paramlist):
         if not self.use_code_sending:
             # send command through the corresponding package call
-            cmd = self.shell + self.args + [self.python_path, "-m", self.package_name] + paramlist
+            if self.is_linux:
+                paramlist = [shlex.quote(p) for p in paramlist]
+            else:
+                if self.is_powershell:
+                    paramlist = [self._powershell_quote(p) for p in paramlist]
+            full_list = [self.python_path, "-m", self.package_name] + paramlist
+            if self.join_params:
+                cmd = self.shell + self.args + [" ".join(full_list)]
+            else:
+                cmd = self.shell + self.args + full_list
+
+            if self.debugging:
+                with open("send_cmd.txt", "w") as f:
+                    for idx, c in enumerate(cmd):
+                        f.write(f"{idx}:{c}$\n")
+
             return self._check_output(cmd)
         else:
             # in code sending mode it is not required that the ipyparallel.cluster.shellcmd
-            # exists (or is update to date) on the 'other' side of the shell. This is partically
+            # exists (or is update to date) on the 'other' side of the shell. This is particular
             # useful when doing further development without copying the adapted file before each
             # test run
-            reciever_code = inspect.getsource(ShellCommandReceive)
-            py_cmd = f"import sys, os\n{reciever_code}\nShellCommandReceive().cmd_{paramlist[0]}("
+            py_cmd = f"import sys, os\n{self.receiver_code}\nShellCommandReceive().cmd_{paramlist[0]}("
             skip = False
             for idx in range(1, len(paramlist)):
                 if skip:
@@ -69,16 +267,10 @@ class ShellCommandSend:
                 if idx > 1:
                     py_cmd += ", "
                 if paramlist[idx][0:2] == "--":
-                    if paramlist[idx+1].isnumeric():
-                        py_cmd += f"{paramlist[idx][2:]}={paramlist[idx+1]}"
-                    else:
-                        py_cmd += f"{paramlist[idx][2:]}=r'{paramlist[idx+1]}'"
+                    py_cmd += f"{paramlist[idx][2:]}={self._format_for_python(paramlist[idx+1])}"
                     skip = True
                 else:
-                    if paramlist[idx].isnumeric():
-                        py_cmd += f"{paramlist[idx]}"
-                    else:
-                        py_cmd += f"r'{paramlist[idx]}'"
+                    py_cmd += self._format_for_python(paramlist[idx])
             py_cmd += ")\n"
             cmd = self.shell + self.args + [self.python_path]
             return check_output(cmd, universal_newlines=True, input=py_cmd)
@@ -111,13 +303,17 @@ class ShellCommandSend:
             if key == "OS-WIN-CMD":
                 system = val
                 shell = "cmd.exe"
+                self.is_powershell = False
                 self.is_linux = False
+                self.join_params = False    # disable joining, since it does not work for windows cmd.exe
             elif key == "OS-WIN-PW":
                 system = val
                 shell = "powershell.exe"
+                self.is_powershell = True
                 self.is_linux = False
             elif key == "OS-LINUX":
                 system = val
+                self.is_powershell = False
                 self.is_linux = True
             elif key == "SHELL":
                 shell = val
@@ -175,9 +371,12 @@ class ShellCommandSend:
         else:
             raise Exception(f"Unexpected output ({output}) returned from by the running shell command")
 
-    def cmd_kill(self, pid):
+    def cmd_kill(self, pid, sig=None):
         """kill remote process with the given pid"""
-        self._send_cmd(["kill", str(pid)])
+        if sig:
+            self._send_cmd(["kill", str(pid), "--sig", str(int(sig))])
+        else:
+            self._send_cmd(["kill", str(pid)])
 
     def cmd_mkdir(self, p):
         """make directory recursively"""
@@ -202,107 +401,9 @@ class ShellCommandSend:
         """delete remote file"""
         output = self._send_cmd(["remove", p])
 
-
-class ShellCommandReceive:
-    """Wrapper for receiving and performing ssh shell commands"""
-    def __init__(self):
-        pass
-
-    def cmd_start(self, start_cmd, env=None, output_file=None):
-        import os
-        #with open("params.txt","w") as f:
-        #    f.write(f"start_cmd={start_cmd},output_file={output_file}-")
-        if os.name == "nt":
-            from subprocess import Popen
-            from subprocess import CREATE_NEW_CONSOLE
-            from subprocess import CREATE_BREAKAWAY_FROM_JOB
-
-            flags = 0
-            flags |= CREATE_NEW_CONSOLE
-            flags |= CREATE_BREAKAWAY_FROM_JOB
-
-            pkwargs = {
-                'close_fds': True,  # close stdin/stdout/stderr on child
-                'creationflags': flags
-            }
-            if output_file:
-                fo = open(output_file, "w")
-                pkwargs['stdout'] = fo
-                pkwargs['stderr'] = fo
-
-            p = Popen(start_cmd, **pkwargs)
-
-            print(f'__remote_pid={p.pid}__')
-        else:
-            print("LINUX BRANCH: NOT IMPLEMENTED YET!!!")
-
-    def cmd_running(self, pid):
-        import os
-        if os.name == "nt":
-            # taken from https://stackoverflow.com/questions/568271/how-to-check-if-there-exists-a-process-with-a-given-pid-in-python
-            import ctypes
-            PROCESS_QUERY_INFROMATION = 0x1000 # if actually PROCESS_QUERY_LIMITED_INFORMATION
-            STILL_ACTIVE = 259
-            processHandle = ctypes.windll.kernel32.OpenProcess(PROCESS_QUERY_INFROMATION, 0, pid)
-            if processHandle == 0:
-                print(f'__running=0__')
-            else:
-                i = ctypes.c_int(0)
-                pi = ctypes.pointer(i)
-                if ctypes.windll.kernel32.GetExitCodeProcess(processHandle, pi) == 0:
-                    print(f'__running=0__')
-                if i.value == STILL_ACTIVE:
-                    print(f'__running=1__')
-                else:
-                    print(f'__running=0__')
-                ctypes.windll.kernel32.CloseHandle(processHandle)
-        else:
-            print("LINUX BRANCH: NOT IMPLEMENTED YET!!!")
-
-    def cmd_kill(self, pid):
-        import os
-        if os.name == "nt":
-            # os.kill doesn't work reliable under windows. also see
-            # https://stackoverflow.com/questions/28551180/how-to-kill-subprocess-python-in-windows
-
-            # solution using taskill
-            #import subprocess
-            #subprocess.call(['taskkill', '/F', '/T', '/PID',  str(pid)])  # /T kills all child processes as well
-
-            # use windows api to kill process (doesn't kill children processes)
-            # To kill all children process things are more complicated. see e.g.
-            # http://mackeblog.blogspot.com/2012/05/killing-subprocesses-on-windows-in.html
-            import ctypes
-            PROCESS_TERMINATE = 0x0001
-            kernel32 = ctypes.windll.kernel32
-            processHandle = kernel32.OpenProcess(PROCESS_TERMINATE, 0, pid);
-            if processHandle:
-                kernel32.TerminateProcess(processHandle, 3)  # 3 is just an arbitrary exit code
-                kernel32.CloseHandle(processHandle)
-        else:
-            print("LINUX BRANCH: NOT IMPLEMENTED YET!!!")
-
-    def cmd_mkdir(self, path):
-        import os
-        os.makedirs(path, exist_ok=True)    # we allow that the directory already exists
-
-    def cmd_rmdir(self, path):
-        import shutil
-        shutil.rmtree(path)
-
-    def cmd_exists(self, path):
-        import os
-        if os.path.exists(path):
-            print("__exists=1__")
-        else:
-            print("__exists=0__")
-
-    def cmd_remove(self, path):
-        import os
-        os.remove(path)
-
 def main():
     parser = ArgumentParser(description='Perform some standard shell command in a platform independent way')
+    parser.add_argument('--debug', action='store_true', help='append command line parameters to \'sys_arg.txt\'')
     subparsers = parser.add_subparsers(dest='cmd', help='sub-command help')
 
     # create the parser for the "a" command
@@ -316,6 +417,7 @@ def main():
 
     parser_kill = subparsers.add_parser('kill', help='kill a process')
     parser_kill.add_argument('pid', type=int, help='pid of process that should be killed')
+    parser_kill.add_argument('--sig', type=int, help='signals to send')
 
     parser_mkdir = subparsers.add_parser('mkdir', help='create directory recursively')
     parser_mkdir.add_argument('path', help='directory path to be created')
@@ -326,7 +428,7 @@ def main():
     parser_exists = subparsers.add_parser('exists', help='checks if a file/directory exists')
     parser_exists.add_argument('path', help='path to check')
 
-    parser_remove = subparsers.add_parser('remove', help='removes as file')
+    parser_remove = subparsers.add_parser('remove', help='removes a file')
     parser_remove.add_argument('path', help='path to remove')
 
     if len(sys.argv) == 1:
@@ -335,6 +437,13 @@ def main():
 
     args = parser.parse_args()
     cmd = args.__dict__.pop('cmd')
+
+    if args.debug:
+        with open("sys_arg.txt", "a") as f:
+            f.write(f"'{__file__}' started in '{os.getcwd()}':\n")
+            for idx, arg in enumerate(sys.argv[1:]):
+                f.write(f"\t{idx}:{arg}$\n")
+    del args.debug
 
     recevier = ShellCommandReceive()
     if cmd == "start":
--- ipyparallel/tests/test_shellcmd.py
+++ ipyparallel/tests/test_shellcmd.py
@@ -1,73 +1,133 @@
-"""test shell command tools"""
-import pickle
-from collections import namedtuple
+"""test shell command classes"""
 
 import pytest
 import sys, os
-import time
+import time, signal
 
 from ipyparallel.cluster import shellcmd
+from subprocess import run
 
 @pytest.fixture
 def setup_shellcmd_senders():
-    senders = []
+
+    senders = []    # each entry is tuple of the shellcmd object and the output file prefix (only needed for wsl to make sure )
     if os.name == 'nt':
-        cmd = shellcmd.ShellCommandSend(["cmd.exe"], ["/C"], sys.executable, use_code_sending=1)
-        ps = shellcmd.ShellCommandSend(["powershell.exe"], ["-Command"], sys.executable)
-        senders = [ps] #, cmd ps]
+        cmd = (shellcmd.ShellCommandSend(["cmd.exe"], ["/C"], sys.executable),  None)
+        cmd_cs = (shellcmd.ShellCommandSend(["cmd.exe"], ["/C"], sys.executable, use_code_sending=1), None)
+        ps = (shellcmd.ShellCommandSend(["powershell.exe"], ["-Command"], sys.executable), None)
+        ps_cs = (shellcmd.ShellCommandSend(["powershell.exe"], ["-Command"], sys.executable, use_code_sending=1), None)
+        ssh = (shellcmd.ShellCommandSend(["ssh"], ["-p", "2222", "ciuser@localhost"], "python", use_code_sending=1), None)
+        #if run(["where", "wsl"]).returncode == 0:
+        #    #if wsl was found we can add a bash test as well (assuming that python3 is also installed)
+        #    bash = (shellcmd.ShellCommandSend(["bash"], ["-c"], "python3", use_code_sending=1), "~/")   # use wsl to test with bash
+        senders = [cmd, cmd_cs, ps, ps_cs, ssh]
+    else:
+        # under linux we could also test more shells
+        bash = (shellcmd.ShellCommandSend(["/usr/bin/bash"], ["-c"], "python3"), None)
+        bash_cs = (shellcmd.ShellCommandSend(["/usr/bin/bash"], ["-c"], "python3", use_code_sending=1), None)
+        ssh = (shellcmd.ShellCommandSend(["ssh"], ["-p", "2222", "ciuser@localhost"], "python3", use_code_sending=1), None)
+        senders = [bash, bash_cs, ssh]
     return senders
 
 @pytest.fixture
 def shellcmd_test_cmd():
     """returns a command that runs for 5 seconds"""
-    if os.name == 'nt':
-        test_command ="ping -n 5 127.0.0.1"
-    else:
-        test_command ="ping -c 5 127.0.0.1"
+    test_command = {}
+    test_command["Windows"] = "ping -n 5 127.0.0.1"
+    test_command["Linux"] = "ping -c 5 127.0.0.1"
     return test_command
-
 
 def test_all_shellcmds(setup_shellcmd_senders, shellcmd_test_cmd):
 
-    for sender in setup_shellcmd_senders:
+    def read_via_shell(shell, filename):
+        # small helper function to read a file via shell commands
+        if shell.is_linux:
+            content = shell.check_output(f"cat {filename}").strip()
+        else:
+            content = shell.check_output(f"type {filename}").strip()
+        content = content.replace("\r\n", "\n") #correct line endings for windows
+        return content.split("\n")
+
+    # go through all senders for testing
+    for sender, prefix in setup_shellcmd_senders:
+        print(f"shell={sender.shell[0]} (code sending={sender.use_code_sending}, joining={sender.join_params}): Start tests...")
+
         info = sender.get_shell_info()
         assert len(info) == 2 and info[0] and info[1]
 
+        if sender.is_linux:
+            test_cmd = shellcmd_test_cmd["Linux"]
+        else:
+            test_cmd = shellcmd_test_cmd["Windows"]
+
         python_ok = sender.check_python()
-        assert python_ok==True
+        assert python_ok is True
 
         test_dir = "shellcmd_test"
-        test_script = "testfile.txt"
+        test_file = "testfile.txt"
 
         # perform basic file/directory operations
         sender.cmd_mkdir(test_dir)
-        assert sender.cmd_exists(test_dir) is True  # make sure that test_dir was created
+        assert sender.cmd_exists(test_dir) is True
 
-        # create a simple text file with one line
-        fullpath = os.path.join(test_dir,test_script)
-        with open(fullpath, "w") as f:
-            f.write("test line\n")
+        # create a simple text file with one line (works on all platforms)
+        fullpath = test_dir+'/'+test_file
+        sender.check_output(f'echo "test-line" > {fullpath}')
 
-        assert sender.cmd_exists(fullpath) is True  # make sure that test file was created
+        assert sender.cmd_exists(fullpath) is True
 
         sender.cmd_remove(fullpath)
-        assert sender.cmd_exists(fullpath) is False  # make sure that test file was deleted
+        assert sender.cmd_exists(fullpath) is False
 
         sender.cmd_rmdir(test_dir)
-        assert sender.cmd_exists(test_dir) is False  # make sure that test_dir was removed
+        assert sender.cmd_exists(test_dir) is False
 
-        # do process operation test
-        redirect_output_file="output.txt"
-        pid = sender.cmd_start(shellcmd_test_cmd, output_file=redirect_output_file)
+        # do start operation test
+        redirect_output_file = "output.txt"
+        pid = sender.cmd_start(test_cmd, output_file=redirect_output_file)
         assert pid > 0
-        assert sender.cmd_running(pid) is True  # make sure that process with pid is running
+        assert sender.cmd_running(pid) is True
 
-        #time.sleep(7)  # ping should run for 5 seconds
-        sender.cmd_kill(pid)
+        sender.cmd_kill(pid, signal.SIGTERM)
 
-        assert sender.cmd_running(pid) is False  # make sure that process with pid is not running any more
-        assert sender.cmd_exists(redirect_output_file) is True  # make sure that output file was created
+        assert sender.cmd_running(pid) is False
+        assert sender.cmd_exists(redirect_output_file) is True
         sender.cmd_remove(redirect_output_file)
 
+        # do environment setting test
+        # we have a dictionary of three environment entries, where the first one is empty
+        env_dict = {"IPP_CLUSTER_ID": "",
+                    "IPP_PROFILE_DIR": r"~/.ipython/profile_ssh",
+                    "IPP_CONNECTION_INFO": {"ssh": "", "interface": "tcp://*", "registration": 60691, "control": 60692, "mux": 60693, "task": 60694, "iopub": 60695, "hb_ping": 60696, "hb_pong": 60697, "broadcast": [60698, 60699], "key": "169b682b-337c645951e7d47723061090", "curve_serverkey": "null", "location": "host", "pack": "json", "unpack": "json", "signature_scheme": "hmac-sha256"},
+                    }
+        # we use a small python script to output provided environment setting
+        python_cmd = "import os;"
+        python_cmd += "print('IPP_CLUSTER_ID set =', 'IPP_CLUSTER_ID' in os.environ);"
+        python_cmd += "print('IPP_PROFILE_DIR =',os.environ['IPP_PROFILE_DIR']);"
+        python_cmd += "print('IPP_CONNECTION_INFO =',os.environ['IPP_CONNECTION_INFO'])"
 
+        output_file = "stdout.txt"
+        if prefix:
+            output_file = prefix + output_file
+        pid = sender.cmd_start(f'{sender.python_path} -c "{python_cmd}"', env=env_dict, output_file=output_file)
 
+        counter = 0
+        max_counter = 5
+        while sender.cmd_running(pid) or counter < max_counter:
+            time.sleep(1)   #sleep for 1 second to make sure that command has finished
+            counter += 1
+
+        assert sender.cmd_running(pid) is False
+
+        output_lines = read_via_shell(sender, output_file)
+        # check lines if they correspond to original dictionary (env_dict)
+        assert len(output_lines) == 3
+        assert output_lines[0] == 'IPP_CLUSTER_ID set = False'
+        for line in output_lines[1:]:
+            key, value = line.split(" = ")
+            assert key in env_dict
+            if isinstance(env_dict[key], dict): # convert to dictionary if needed
+                value = eval(value)
+            assert env_dict[key] == value
+
+        print(f"shell={sender.shell[0]} (code sending={sender.use_code_sending}): tests finished successful")
